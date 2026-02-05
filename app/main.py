1df58b042ae22ab3e576144b3d9958bca3c032f0
gunsts <137734013+gunsts@users.noreply.github.com>
2026-02-05 21:15:15 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,67 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers"
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+    def __iadd__(self, other) -> Distance:
+        if isinstance(other, Distance):
+            self.km += other.km
+        else:
+            self.km += other
+        return self
+
+    def __mul__(self, other) -> Distance:
+        if isinstance(other, Distance):
+            self.km = self.km * other.km
+        else:
+            self.km = self.km * other
+        return self
+
+    def __truediv__(self, other) -> Distance:
+        if isinstance(other, Distance):
+            self.km = round(self.km / other.km, 2)
+        else:
+            self.km = round(self.km / other, 2)
+        return self
+
+    def __lt__(self, other) -> Distance:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        else:
+            return self.km < other
+
+    def __gt__(self, other) -> Distance:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        else:
+            return self.km > other
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        else:
+            return self.km == other
+
+    def __le__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        else:
+            return self.km <= other
+
+    def __ge__(self, other) -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        else:
+            return self.km >= other
